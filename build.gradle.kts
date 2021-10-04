00a98ea23756406d5df5c6df815c94e433f659f7
mandelshtamd <Ofwgktadgaf1984>
2021-10-04 11:35:26 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.20"
+    application
+}
+
+group = "me.owner"
+version = "1.0"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-netty:1.6.0")
+    implementation("io.ktor:ktor-html-builder:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "ServerKt"
+}
